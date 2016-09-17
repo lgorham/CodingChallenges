1b5bf308cb9a3f95c1deb29a12e5516ad879c18e
Lydia Gorham <gorhamlydia@gmail.com>
2016-09-17 17:46:15 +0000

    Recursively dump arbitrary depth

---
+++ split-square/dump.py
@@ -0,0 +1,68 @@
+"""Print each square on a new line.
+
+A simple square will only be one line::
+
+    >>> dump(0)
+    0
+
+    >>> dump(1)
+    1
+
+A split square will use four lines::
+
+    >>> dump([0, 1, 0, 1])
+    0
+    1
+    0
+    1
+
+A nested split square will use one line per square::
+
+    >>> dump([0, 0, 0, [1, 1, 1, 1]])
+    0
+    0
+    0
+    1
+    1
+    1
+    1
+
+Of course, these can nested deeply and still work::
+
+    >>> dump([0, 0, 0, [1, 1, 1, [0, 0, 0, [1, 1, 1, 1]]]])
+    0
+    0
+    0
+    1
+    1
+    1
+    0
+    0
+    0
+    1
+    1
+    1
+    1
+"""
+
+
+def dump(square):
+    """Print each square on a new line."""
+
+    if isinstance(square, int):
+        print square
+        return
+        
+    for i in square:
+        if isinstance(i, list):
+            dump(i)
+        else:
+            print i
+
+    
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASS; NICE JOB!\n"
