39085c65633925283a87538b4228f8d1bdab13cb
Lydia Gorham <gorhamlydia@gmail.com>
2016-09-18 21:27:51 +0000

    Simple pre-sorted merge sort

---
+++ sortab.py
@@ -0,0 +1,50 @@
+"""Given already-sorted lists, `a` and `b`, return sorted list of both.
+
+You may not use sorted() or .sort().
+
+Check edge cases of empty lists:
+
+    >>> sort_ab([], [])
+    []
+
+    >>> sort_ab([1, 2,3], [])
+    [1, 2, 3]
+
+    >>> sort_ab([], [1, 2, 3])
+    [1, 2, 3]
+
+Check:
+
+    >>> sort_ab([1, 3, 5, 7], [2, 6, 8, 10])
+    [1, 2, 3, 5, 6, 7, 8, 10]
+"""
+
+
+def sort_ab(a, b):
+    """Given already-sorted lists, `a` and `b`, return sorted list of both.
+
+    You may not use sorted() or .sort().
+    """
+
+    merged = []
+    while a and b:
+        if a[0] < b[0]:
+            merged.append(a[0])
+            a.pop(0)
+        elif b[0] < a[0]:
+            merged.append(b[0])
+            b.pop(0)
+
+    if a:
+        merged.extend(a)
+
+    if b:
+        merged.extend(b)
+
+    return merged
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. YOU'RE A MERGE CHAMPION!!\n"
